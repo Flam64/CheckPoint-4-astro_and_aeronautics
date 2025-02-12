76dc956206f9ce0a0dc101e857ccd734f7f2b033
Flam64 <celio.mozes.rocha@gmail.com>
2025-02-12 22:57:57 +0100

    feat: mise en place de la page d'accueil US#12

---
+++ client/src/router.tsx
@@ -0,0 +1,21 @@
+import App from "./App";
+
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+
+export const mainRouter = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+]);
