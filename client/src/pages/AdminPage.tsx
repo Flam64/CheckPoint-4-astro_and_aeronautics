7afa05f614127e3cc7951c7d8cc0b11d37125d34
Flam64 <celio.mozes.rocha@gmail.com>
2025-02-14 18:15:48 +0100

    feat: update page admin -login and register user - US#3

--- client/src/pages/AdminPage.tsx
+++ client/src/pages/AdminPage.tsx
@@ -3,27 +3,27 @@ import { Bounce, ToastContainer } from "react-toastify";
 import NavBarAdmin from "../components/Admin/NavBarAdmin";
 
 export default function AdminPage() {
-	return (
-		<>
-			<header>
-				<NavBarAdmin />
-			</header>
-			<main>
-				<Outlet />
-			</main>
-			<ToastContainer
-				position="top-center"
-				autoClose={6000}
-				hideProgressBar={true}
-				newestOnTop={false}
-				closeOnClick={false}
-				rtl={false}
-				pauseOnFocusLoss
-				draggable
-				pauseOnHover
-				theme="light"
-				transition={Bounce}
-			/>
-		</>
-	);
+  return (
+    <>
+      <header>
+        <NavBarAdmin />
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <ToastContainer
+        position="top-center"
+        autoClose={6000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
+    </>
+  );
 }
